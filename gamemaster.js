8f07310b84968f2c66777403cf35bd78af1f3681
seasonsb <snowb400@gmail.com>
2014-12-24 15:43:49 -0500

    Testing Things

--- gamemaster.js
+++ gamemaster.js
@@ -1,2 +1,6 @@
-var canvas = document.getElementById("gamescreen");
+var canvas = document.createElement("canvas");
+canvas.setAttribute("id", "gamescreen");
+document.body.appendChild(canvas);
 var ctx = canvas.getContext("2d");
+ctx.fillStyle = "green";
+ctx.fillRect(0,0,100,100);
