691f5493ef1b99c42cfc8c70fb76d1041455df9d
jerrickc <chenjerrick@gmail.com>
2015-01-04 22:13:02 -0500

    Removed unnecessary bit
    Changed code I found online to fit what I needed

--- gamemaster.js
+++ gamemaster.js
@@ -38,8 +38,8 @@ $("#resetter").on("mousedown", function() {
     restartGame();
 });
 window.addEventListener("keydown", function(e) {
-    // space and arrow keys
-    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+    // no scrolling by arrow keys
+    if([37, 38, 39, 40].indexOf(e.keyCode) > -1) {
         e.preventDefault();
     }
 }, false);
